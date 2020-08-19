c3ff89dcd4974f98c5577a820fc883ac5e7c70f6
anadoh <68846949+anadoh@users.noreply.github.com>
2020-08-19 15:10:48 +0200

    JSON_response_url_show_url

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
 const init = function () {
-  const searchButton = document.querySelector('.search__button');
-  searchButton.addEventListener('click', handleUrl);
+  const searchButton = document.querySelector('#form');
+  searchButton.addEventListener('submit', handleUrl);
 }
 
 const handleUrl = function (event) {
-  console.log(event);
-  alert("ok");
+  event.preventDefault();
+  const url = document.querySelector(".form_container").value;
+  shortUrl(url);
+  
 }
 
+
+const showUrl = function (response, url) {
+  let counter = 0;
+  const newEl = document.createElement("div");
+  newEl.innerText =` ${url} ${++counter}`;
+  const div = document.querySelector(".url"); 
+  div.appendChild(newEl);
+  
+  let counter2 = 0;
+  const newEl2 = document.createElement("div");
+  newEl2.innerText =`https://rel.ink/ ${response.hashid} ${++counter2}`;
+  
+  const div2 = document.querySelector(".url_short"); 
+  const strong =div2.querySelector(".url_button");
+  div2.insertBefore(newEl2,strong);
+
+}
+
+const shortUrl = function(url) {
+  console.log(url, "url")
+  fetch('https://rel.ink/api/links/', {
+    method: 'POST',
+    body: JSON.stringify({"url": url}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then (response => {
+    console.log('respose ', response);
+    if (response.ok) {
+      console.log('respose2 ', response);
+      return response.json();
+    }
+    throw new Error ('Request failed!');
+  }, networkError => console.log(networkError.message)
+  ).then(jsonResponse => {console.log("success", jsonResponse)
+    showUrl(jsonResponse, url)
+  })
+}
 
 window.onload = function() {
   init();
