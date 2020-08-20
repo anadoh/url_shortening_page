a5053d8176abd51b4575f782a7e0c1724257c2c3
anadoh <68846949+anadoh@users.noreply.github.com>
2020-08-20 15:41:02 +0200

    mouse_event copy

--- main.js
+++ main.js
@@ -12,19 +12,41 @@ const handleUrl = function (event) {
 
 
 const showUrl = function (response, url) {
+  const redirectUrl = `https://rel.ink/${response.hashid}`;
+
+
   const newEl = document.createElement("div");
   newEl.innerText =` ${url}`;
   const div = document.querySelector(".url"); 
   div.appendChild(newEl);
   
- 
+
   const newEl2 = document.createElement("div");
-  newEl2.innerText =`https://rel.ink/ ${response.hashid}`;
+  newEl2.innerText = redirectUrl;
   const div2 = document.querySelector(".url_short"); 
   div2.appendChild(newEl2);
+  
 
   const newEl3 = document.createElement("button");
   newEl3.innerText =`Copy`;
+
+ newEl3.onmouseover = function () {
+    newEl3.style.backgroundColor = "hsl(180, 57%, 72%)";
+    newEl3.onmouseout = function () {
+      newEl3.style.backgroundColor = "hsl(180, 66%, 49%)";
+    }
+  }
+
+  newEl3.onclick = function () {
+    newEl3.innerText =`Copied!`;
+    newEl3.style.backgroundColor = "hsl(260, 38%, 28%)";
+    copyToClipboard(redirectUrl);
+    this.onmouseout = null;
+    this.onmouseover = null;
+  } 
+
+ 
+    
 
   const div3 = document.createElement("div")
   div3.appendChild(newEl3);
@@ -33,9 +55,17 @@ const showUrl = function (response, url) {
   button.appendChild(div3);
 
 
-  
-
 }
+
+function copyToClipboard(text) {
+  const elem = document.createElement('textarea');
+  elem.value = text;
+  document.body.appendChild(elem);
+  elem.select();
+  document.execCommand('copy');
+  document.body.removeChild(elem);
+}
+
 
 const shortUrl = function(url) {
   console.log(url, "url")
@@ -57,6 +87,8 @@ const shortUrl = function(url) {
     showUrl(jsonResponse, url)
   })
 }
+
+
 
 window.onload = function() {
   init();
